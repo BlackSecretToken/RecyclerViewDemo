41ab213257fe6408903aa6dd5f6fecdbff5e5d62
blacksecrettoken <secretsuperdev9472@gmail.com>
2023-06-14 12:04:59 +1000

    add button

--- app/src/main/java/com/example/myrecyclerviewdemo/MyViewHolder.java
+++ app/src/main/java/com/example/myrecyclerviewdemo/MyViewHolder.java
@@ -1,21 +1,35 @@
 package com.example.myrecyclerviewdemo;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyViewHolder extends RecyclerView.ViewHolder{
+public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
     ImageView imageView;
     TextView nameView, emailView;
+    Button shareButton;
 
     public MyViewHolder(@NonNull View itemView){
         super(itemView);
         imageView = itemView.findViewById(R.id.imageview);
         nameView = itemView.findViewById(R.id.name);
         emailView = itemView.findViewById(R.id.email);
+        shareButton = itemView.findViewById(R.id.shareButton);
+        shareButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == shareButton) {
+            int position = getAdapterPosition();
+            // Do something with this position, like remove the item from the list
+            Log.d("tag","-------------------share button ----------------------");
+        }
     }
 }
